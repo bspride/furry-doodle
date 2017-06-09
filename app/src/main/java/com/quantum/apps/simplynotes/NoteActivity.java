580a94637ae5c774797c922fb852ff5a2772d36a
Clarke <mgclarke@186590d3c201.ant.amazon.com>
2017-06-08 23:57:56 -0700

    Change from actionbar to toolbars to support buttons in ActionBar

--- app/src/main/java/com/quantum/apps/simplynotes/NoteActivity.java
+++ app/src/main/java/com/quantum/apps/simplynotes/NoteActivity.java
@@ -3,9 +3,13 @@ package com.quantum.apps.simplynotes;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -24,13 +28,37 @@ public class NoteActivity extends AppCompatActivity {
 
         noteRepository = new NoteRepository(this);
 
-        markdownView = (MarkdownView) findViewById(R.id.markdownView);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.note_action_bar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        this.markdownView = (MarkdownView) findViewById(R.id.markdownView);
+
 
         Intent intent = getIntent();
         long id = intent.getLongExtra(NOTE_KEY, -1);
 
         if (id != -1)
-            getNoteData(id);
+            markdownView.loadMarkdown(getNoteData(id));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_edit_note) {
+            int x = 99;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -39,9 +67,9 @@ public class NoteActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void getNoteData(long id) {
+    private String getNoteData(long id) {
         Note note = (Note) noteRepository.item(id);
-
-        markdownView.loadMarkdown(note.getData());
+        String encodedData = note.getData().replaceAll("(\n)", "<br />");
+        return encodedData;
     }
 }
